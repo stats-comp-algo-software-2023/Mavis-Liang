91f4ef9e3ab7643c26cb6cd86dfaf8518b773b4b
Mavis Liang <liangxw27@gmail.com>
2023-02-13 16:22:47 -0500

    add helper function for testing

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -3,25 +3,29 @@
 #' @export vcov.hglm
 #' @export print.hglm
 
-hiper_glm <- function(design, outcome){
+hiper_glm <- function(design, outcome, model='linear'){
+  supported_models <- c('linear')
+  if(!(model %in%  supported_models)){stop(sprintf("The model %s is not supported.", model))
+}
+  
   warning("The function is yet to be implemented")
-  ##TODO: 
+  ## TODO: 
   hglm_out <- list()
   class(hglm_out) <- "hglm"
   hglm_out
 }
 
 
-coef.hglm <- function(obj){
+coef.hglm <- function(hglm_out){
   return("This is the coefs")
 }
 
 
-vcov.hglm <- function(obj){
+vcov.hglm <- function(hglm_out){
   return("This is the matrix")
 }
 
-print.hglm <- function(obj){
+print.hglm <- function(hglm_out){
   return("printing...")
 }
 
